641ce311765ab560e60b0e31baf490f9d244e980
Clement-R <rondeau.clement@gmail.com>
2016-04-19 14:02:22 +0200

    Add TiledMapTest project

---
+++ Assets/TiledMapTest/Scripts/JumpJumpJump.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ScreenShaking
+{
+    public class JumpJumpJump : MonoBehaviour
+    {
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+                gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 5, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/TiledMapTest/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Parallax
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        public int speed = 10;
+        public int jumpVelocity = 5;
+
+        void Update()
+        {
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+
+            if(Input.GetKeyDown(KeyCode.Space))
+                gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+        }
+    }
+}
+
+
