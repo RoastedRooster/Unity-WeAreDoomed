d9e892af88734d8400d91785d69ef733e1024bbb
Nujerk <junkerlucas@gmail.com>
2016-04-22 23:59:45 +0200

    UI Tests on level start

---
+++ Assets/UITests/Scripts/UILevelGo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UILevelGo : MonoBehaviour {
+
+    public GameObject canvas;
+
+    public void FixedUpdate()
+    {
+        if (Input.anyKeyDown || AxisMoved())
+        {
+            canvas.GetComponent<Animator>().Play("End");
+            gameObject.SetActive(false);
+        }
+            
+    }
+
+    private bool AxisMoved()
+    {
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.7 || Mathf.Abs(Input.GetAxis("Vertical")) > 0.7)
+            return true;
+
+        return false;
+    }
+}
