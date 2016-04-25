b1a380d32aded121693967c65a07f7eeba6a4780
Clement-R <rondeau.clement@gmail.com>
2016-04-25 08:17:00 +0200

    Rework PlayerController script to move with forces. It solves the bug of character going through platforms.

--- Assets/TiledMapTest/Scripts/PlayerController.cs
+++ Assets/TiledMapTest/Scripts/PlayerController.cs
@@ -6,11 +6,21 @@ namespace Parallax
     public class PlayerController : MonoBehaviour
     {
 
-        public int speed = 10;
+        public float speed = 10;
+        public float maxSpeed = 5f;
+        public float moveForce = 365f;
         public int jumpVelocity = 5;
+
+
+        private Rigidbody2D rb2d;
+
+        void Awake() {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
 
         void Update()
         {
+            /*
             if(Input.GetKey(KeyCode.LeftArrow))
             {
                 transform.position += Vector3.left * speed * Time.deltaTime;
@@ -19,9 +29,20 @@ namespace Parallax
             {
                 transform.position += Vector3.right * speed * Time.deltaTime;
             }
+            */
 
             if(Input.GetKeyDown(KeyCode.Space))
                 gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+        }
+
+        void FixedUpdate() {
+            float h = Input.GetAxis("Horizontal");
+
+            if (h * rb2d.velocity.x < maxSpeed)
+                rb2d.AddForce(Vector2.right * h * moveForce);
+
+            if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+                rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
         }
     }
 }
